82b5e89283e00d9ae501ac1d027003d049a82642
SHIFT969 <ivan000maslov@gmail.com>
2023-08-27 16:09:07 +0200

    Base and base scripts

---
+++ Assets/Scripts/TileState.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+[CreateAssetMenu(menuName = "Tile State")]
+public class TileState : ScriptableObject
+{
+    public Color backgroundColor;
+
+    public Color textColor;
+}
